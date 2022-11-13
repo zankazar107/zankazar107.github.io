73b571b7e4040236ac4a9eae2680faa4dc03a965
zankazar107 <76654230+zankazar107@users.noreply.github.com>
2022-11-13 03:52:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require('express')();
+const app = require('express');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
